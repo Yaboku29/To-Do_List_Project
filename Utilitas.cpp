1f61ddf45fca2816e179b4c6d3ccc804f3a9b2e9
Ananda Rizky <anandarizkysetyanu29@gmail.com>
2025-03-29 22:10:12 +0700

    update header

--- Utilitas.cpp
+++ Utilitas.cpp
@@ -5,21 +5,18 @@ using namespace std;
 void tampilkanHeader(){
     cout<<CYAN<<BOLD;
     cout << R"(  
-        _______   ___   ____    ___
-       |__   __| / _ \ |  _ \  / _ \
-          | |   | | | || | | || | | |
-          | |   | |_| || |_| || |_| |
-          |_|    \___/ |____/  \___/
-        d888888b  .d88b.  d8888b.  .d88b.  
-        ```88``` d8P  Y8b 88   8D d8P  Y8b
-           88    8P    Y8 88   88 8P    Y8
-           88    8b    d8 88   88 8b    d8
-           88    Y8b   dB 88   8D Y8b   dB
-           YP     `Y88P'  Y8888D`  `Y88P'
+         _________    _____    ______     _____
+        |___   ___|  /     \  |      \   /     \
+            | |     |  ___  | |  ___  | |  ___  |
+            | |     | |   | | | |   | | | |   | |
+            | |     | |   | | | |   | | | |   | |
+            | |     | |___| | | |___| | | |___| |
+            |_|      \_____/  |______/   \_____/ 
           Made by:
           )";
-    cout<<RED;
-    cout<<"Yaboku29";
+    cout<<RED<<"Yaboku29";
+    
+    
     cout << RESET << endl;  // Kembali ke warna default
 }
 
