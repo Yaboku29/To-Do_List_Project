ecd33dcf8d269d70af50d802bfb56f08b92278da
Ananda Rizky <anandarizkysetyanu29@gmail.com>
2025-03-29 18:29:17 +0700

    revisi kecil

--- Utilitas.cpp
+++ Utilitas.cpp
@@ -5,12 +5,15 @@ using namespace std;
 void tampilkanHeader(){
     cout<<CYAN<<BOLD;
     cout << R"(  
-        _______   ___   ____     ___
-       |__   __| / _ \ |  _ \   / _ \
-          | |   | | | || | | | | | | |
-          | |   | |_| || |_| | | |_| |
-          |_|    \___/ |____/   \___/
-      )";
+        _______   ___   ____    ___
+       |__   __| / _ \ |  _ \  / _ \
+          | |   | | | || | | || | | |
+          | |   | |_| || |_| || |_| |
+          |_|    \___/ |____/  \___/
+        Made by:
+          )";
+    cout<<RED;
+    cout<<"Yaboku29"<<endl;
       
     cout << RESET << endl;  // Kembali ke warna default
 }
