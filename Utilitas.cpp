ed1251d4a5f2f9b8b878e12e08f0835c99a5129e
Ananda Rizky <anandarizkysetyanu29@gmail.com>
2025-03-29 18:01:11 +0700

    tambahkan fungsi pada utilitas

---
+++ Utilitas.cpp
@@ -0,0 +1,14 @@
+#include "Utilitas.hpp"
+
+using namespace std;
+
+void tampilkanHeader(){
+    cout<<CYAN<<BOLD;
+    cout << "████████╗ ██████╗     ██████╗  ██████╗ \n";
+    cout << "╚══██╔══╝██╔═══██╗    ██╔══██╗██╔═══██╗\n";
+    cout << "   ██║   ██║   ██║    ██║  ██║██║   ██║\n";
+    cout << "   ██║   ██║   ██║    ██║  ██║██║   ██║\n";
+    cout << "   ██║   ╚██████╔╝    ██████╔╝╚██████╔╝\n";
+    cout << "   ╚═╝    ╚═════╝     ╚═════╝  ╚═════╝ \n";
+    cout << RESET << endl;  // Kembali ke warna default
+}
