a7e0d7f23a7104a8dd9e3146eefbac3e2e7cf916
Ananda Rizky <anandarizkysetyanu29@gmail.com>
2025-03-29 21:58:24 +0700

    coba header

--- Utilitas.cpp
+++ Utilitas.cpp
@@ -10,7 +10,13 @@ void tampilkanHeader(){
           | |   | | | || | | || | | |
           | |   | |_| || |_| || |_| |
           |_|    \___/ |____/  \___/
-        Made by:
+        d888888b  .d88b.  d8888b.  .d88b.  
+        ```88``` d8P  Y8b 88   8D d8P  Y8b
+           88    8P    Y8 88   88 8P    Y8
+           88    8b    d8 88   88 8b    d8
+           88    Y8b   dB 88   8D Y8b   dB
+           YP     `Y88P'  Y8888D`  `Y88P'
+          Made by:
           )";
     cout<<RED;
     cout<<"Yaboku29";
