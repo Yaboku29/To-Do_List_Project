ba07299aa9b2dbe101b1559e0b628f13edc6bad0
Ananda Rizky <anandarizkysetyanu29@gmail.com>
2025-03-30 12:47:33 +0700

    update menu

--- main.cpp
+++ main.cpp
@@ -3,10 +3,11 @@
 #include "file_io.hpp"
 
 // void setColor(const string &color);
-void menu();
+void menu1();
+void menu2();
 
 int main(){
-    menu();
+    menu1();
     return 0;
 }
 
@@ -14,7 +15,7 @@ int main(){
 //     cout<<color;
 // }
 
-void menu(){
+void menu1(){
     clearScreen();
     int pilihan;
     //cout<<BLUE_BG;
@@ -24,13 +25,9 @@ void menu(){
     cout<<"2. Tambah Tugas\n";
     cout<<"3. Hapus Tugas\n";
     cout<<"4. Edit tugas\n";
-    cout<<"5. Ubah status\n";
-    cout<<"6. Cari Tugas\n";
-    cout<<"7. Filter Tugas\n";
-    cout<<"8. Export File To-Do List\n";
-    cout<<"9. Import File To-Do List\n";
+    cout<<"5. Slide 2\n";
     setColor(RED);
-    cout<<"10. Keluar\n";
+    cout<<"6. Keluar\n";
     setColor(GREEN);
     cout<<"Pilih menu: ";
     cin>>pilihan;
@@ -40,55 +37,95 @@ void menu(){
     case 1:
         tampilkan_tugas();
         system("pause");
-        menu();
+        menu1();
         break;
     case 2:
         tambah_tugas();
         system("pause");
-        menu();
+        menu1();
         break;
     case 3:
         hapus_tugas();
         system("pause");
-        menu();
+        menu1();
         break;
-    case 10:
+    case 6:
         cout<<"Keluar dari program...\n";
         break;
     case 4:
         edit_tugas();
         system("pause");
-        menu();
+        menu1();
         break;
     case 5:
-        tandai_selesai();
+        menu2();
         system("pause");
-        menu();
-        break;
-    case 6:
-        cari_tugas();
-        system("pause");
-        menu();
-        break;
-    case 7:
-        filter_tugas();
-        system("pause");
-        menu();
-        break;
-    case 8:
-        eksporData();
-        system("pause");
-        menu();
-        break;
-    case 9:
-        imporData();
-        system("pause");
-        menu();
+        menu1();
         break;
     default:
         cout<<"Pilihan tidak valid, coba lagi.\n";
         system("pause");
-        menu();
+        menu1();
+        break;
+    }
+}
+
+void menu2(){
+    clearScreen();
+    int pilihan;
+    //cout<<BLUE_BG;
+    cout<<"===>>> TO-DO LIST <<<===\n";
+    setColor(BLUE);
+    cout<<"5. Ubah status\n";
+    cout<<"6. Cari Tugas\n";
+    cout<<"7. Filter Tugas\n";
+    cout<<"8. Export File To-Do List\n";
+    cout<<"9. Import File To-Do List\n";
+    cout<<"10. Balik ke slide 1\n";
+    setColor(RED);
+    cout<<"11. Keluar\n";
+    setColor(GREEN);
+    cout<<"Pilih menu: ";
+    cin>>pilihan;
+
+    switch (pilihan)
+    {
+    case 11:
+        cout<<"Keluar dari program...\n";
+        break;
+    case 5:
+        tandai_selesai();
+        system("pause");
+        menu1();
+        break;
+    case 6:
+        cari_tugas();
+        system("pause");
+        menu1();
+        break;
+    case 7:
+        filter_tugas();
+        system("pause");
+        menu1();
+        break;
+    case 8:
+        eksporData();
+        system("pause");
+        menu1();
+        break;
+    case 9:
+        imporData();
+        system("pause");
+        menu1();
+        break;
+    case 10:
+        system("pause");
+        menu1();
+        break;
+    default:
+        cout<<"Pilihan tidak valid, coba lagi.\n";
+        system("pause");
+        menu1();
         break;
     }
 }
